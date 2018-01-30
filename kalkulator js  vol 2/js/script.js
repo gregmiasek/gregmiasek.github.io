2ac043080e1ee93f70f1adb7a481a70df6a28111
gregmiasek <gmiasek@gmail.com>
2018-01-30 17:16:13 +0100

    poniedzialek

--- kalkulator js  vol 2/js/script.js
+++ kalkulator js  vol 2/js/script.js
@@ -1,11 +1,9 @@
-var numer1 = [];
-var digit1 = 0;
-var suma = [];
-var roznica = [];
-var iloczyn = [];
-var iloraz = [];
 
-document.getElementById('btn-0').addEventListener('click', function () {
+var numer1 = [];
+var numer2 = [];
+var digit1 = 0;
+
+document.getElementById('btn-0').addEventListener('click', function(){
     numer1.push(0);
     var digit1 = Number(numer1.join(''));
     document.getElementById('screen').value = digit1;
@@ -86,60 +84,57 @@ document.getElementById('btn-dot').addEventListener('click', function () {
 
 document.getElementById('btn-add').addEventListener('click', function () {
     var digit1 = Number(numer1.join(''));
-    suma.push(digit1);
+    var digit2 = Number(numer2.join(''));
+    sum = eval(digit1 + digit2);
+    numer2.length = 0;
+    numer2.push(sum);
     numer1.length = 0;
-    console.log(suma);
+    console.log(digit1);
+    console.log(sum + ' suma');
 });
 
-document.getElementById('btn-slash').addEventListener('click', function () {
-
-    var digit1 = Number(numer1.join(''));
-    iloraz.push(digit1);
-    numer1.length = 0;
-    console.log(iloraz);
-
-})
+document.getElementById('btn-slash').addEventListener('click', function () {})
 
 document.getElementById('btn-min').addEventListener('click', function () {
     var digit1 = Number(numer1.join(''));
-    roznica.push(digit1);
+    var digit2 = Number(numer2.join(''));
+    dif = digit2 - digit1;
+    numer2.length = 0;
+    numer2.push(dif);
     numer1.length = 0;
-    console.log(roznica);
+    console.log(digit1);
+    console.log(dif + ' roznica');
 
 });
 
 document.getElementById('btn-prod').addEventListener('click', function () {
     var digit1 = Number(numer1.join(''));
-    iloczyn.push(digit1);
+    var digit2 = Number(numer2.join(''));
+    ilocz = digit1 * digit2;
+    numer2.length = 0;
+    numer2.push(ilocz);
     numer1.length = 0;
-    console.log(iloczyn);
+    console.log(digit1);
+    console.log(ilocz + ' iloczyn');
 });
 
+
 document.getElementById('btn-eq').addEventListener('click', function () {
-    var sum = 0;
-    for (var i = 0; i < suma.length; i++) {
-        sum += suma[i]
-    }
-
-
-    console.log(sum);
-    console.log(roz);
-
-
-
-
+    console.log(s);
     numer1.length = 0;
-    suma.length = 0;
-    iloczyn.length = 0;
-    iloraz.length = 0;
-    roznica.length = 0;
+    console.log(digit1);
+    numer1.length = 0;
+    digit1.length = 0;
+    wynik.length = 0;
+
 });
 
 document.getElementById('btn-del').addEventListener('click', function () {
     document.getElementById('screen').value = '';
     numer1.length = 0;
-    suma.length = 0;
-    iloczyn.length = 0;
-    iloraz.length = 0;
-    roznica.length = 0;
+    numer2.length = 0;
+    sum = 0;
+    dif = 0;
+    divi = 0;
+    ilocz = 0;
 });
