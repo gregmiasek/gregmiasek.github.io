a59a395fd732ee6c2e25455204f94e53f74405fa
gregmiasek <gmiasek@gmail.com>
2018-01-31 19:29:15 +0100

    animowany kwadrat

---
+++ jq animacja kwadratu/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    console.log("ready!");
+
+
+    $('input').click(function () {
+            $('div').animate({
+                left: '100px',
+                width: '100px',
+                height: '100px'
+            }, 3000);
+
+
+            function tlo() {
+                $('div').animate({
+                   backgroundColor: "rgb( 20, 20, 20 )"
+                }, 5000)
+            }
+
+
+            setTimeout(tlo, 3000);
+
+            function header() {
+                $('div').html('<h2>ANIMACJA ZAKOŃCZONA</h2>');
+            }
+
+            setTimeout(header, 8000);
+
+        }
+
+    )
+
+});
