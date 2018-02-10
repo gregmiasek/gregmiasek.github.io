3ba43348274b1b2384decadbfb39c1305f479e3b
gregmiasek <gmiasek@gmail.com>
2018-02-10 12:01:49 +0100

    portfolio blue

--- MOJE PORTFOLIO/js/main.js
+++ MOJE PORTFOLIO/js/main.js
@@ -2,11 +2,26 @@
 
 var navListItem = document.getElementsByClassName('navli');
 var beep = document.getElementById('beep');
-
-console.log(navListItem);
+var muteIc = document.getElementById('volume-icon');
+var amient = document.getElementById('ambient');
 
 for (var i = 0; i < navListItem.length; i++) {
     navListItem[i].addEventListener('mouseenter', function () {
         beep.play();
     });
 }
+
+var muteBtn = document.getElementById('volume');
+
+muteBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (muteIc.classList.contains("fa-volume-up")) {
+        muteIc.classList.remove("fa-volume-up");
+        muteIc.classList.add("fa-volume-off");
+        ambient.muted = true;
+    } else {
+        muteIc.classList.remove("fa-volume-off");
+        muteIc.classList.add("fa-volume-up");
+        ambient.muted = false;
+    }
+});
