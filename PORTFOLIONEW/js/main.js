2f1dbac9b2215c32814e0d1b7b1cfb7ed38e614f
gregmiasek <gmiasek@gmail.com>
2018-03-05 22:54:15 +0100

    portfolio nowe layout

--- PORTFOLIONEW/js/main.js
+++ PORTFOLIONEW/js/main.js
@@ -5,6 +5,10 @@ $(document).ready();
 var sections = document.getElementsByTagName('section');
 console.log(sections);
 
+for (var i = 0; i < sections.length; i++) {
+    sections[i].addEventListener('click', preview);
+}
+
 function preview() {
 
     for (var i = 0; i < sections.length; i++) {
@@ -15,7 +19,6 @@ function preview() {
     var ident = $('section').index(this);
     $('section').removeClass('appear');
     $('section').addClass('vanished');
-    $('footer').addClass('vanished');
     $('#larger').remove;
     $('body').append('<div class="preveiwer" id="larger"></div>');
     var framer = document.getElementById('larger');
@@ -24,23 +27,18 @@ function preview() {
 
     $('#close').click(function () {
         $('section').removeClass('vanished');
-        $('footer').removeClass('vanished');
         $('section').addClass('appear');
-        $('footer').addClass('appear');
         $(framer).addClass('vanished');
 
         setTimeout(function () {
             $(framer).remove();
             for (var i = 0; i < sections.length; i++) {
-            sections[i].addEventListener('click', preview);
-        }
+                sections[i].addEventListener('click', preview);
+            }
         }, 2000);
 
-        
+
     });
 
 }
 
-for (var i = 0; i < sections.length; i++) {
-    sections[i].addEventListener('click', preview);
-}
